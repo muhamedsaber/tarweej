1aff1d8f5594709b97a3f369f9457c75e8890b5d
saber son <elmohandesstamera@gmail.com>
2024-06-27 12:49:18 +0300

    feat: Add Firebase configuration files for iOS, macOS, and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tarweej/config/connectivity/connectivity_controller.dart';
+import 'package:tarweej/firebase_options.dart';
 import 'package:tarweej/tarweej_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await ConnectivityController.instance.init();
   runApp(const TarweejApp());
 }
